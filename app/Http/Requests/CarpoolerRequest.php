024041041068a434a50837fdcb8a8411047b3097
Ibuki-Narukawa <ibuki0212job@gmail.com>
2021-12-13 15:03:06 +0900

    バリデーション修正　緯度経度追加

--- app/Http/Requests/CarpoolerRequest.php
+++ app/Http/Requests/CarpoolerRequest.php
@@ -27,6 +27,10 @@ class CarpoolerRequest extends FormRequest
             'start_datetime' => 'required|after_or_equal:now',
             'from' => 'required|string|max:50',
             'to' => 'required|string|max:50',
+            'latFrom' => 'required|numeric',
+            'lngFrom' => 'required|numeric',
+            'latTo' => 'required|numeric',
+            'lngTo' => 'required|numeric',
         ];
     }
     
@@ -37,6 +41,10 @@ class CarpoolerRequest extends FormRequest
             'start_datetime.after_or_equal' => '出発日時は現在時刻以降にしてください。',
             'from.required' => '出発地は必ず入力してください。',
             'to.required' => '目的地は必ず入力してください。',
+            'latFrom.required' => '出発地が取得できませんでした。もう一度入力してください。',
+            'lngFrom.required' => '出発地が取得できませんでした。もう一度入力してください。',
+            'latTo.required' => '目的地が取得できませんでした。もう一度入力してください。',
+            'lngTo.required' => '目的地が取得できませんでした。もう一度入力してください。',
         ];
     }
 }
