5cbe85168703e8d82ba61f08e45978badd49ab9c
Ibuki-Narukawa <ibuki0212job@gmail.com>
2021-12-31 00:39:31 +0900

    api連携　変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Scripts -->
     <!--<script src="https://js.stripe.com/v3/"></script>-->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://maps.google.com/maps/api/js?v=3&key=AIzaSyCfvExhGV8g1FmBMPfttIf-og7-Q7aFfMc&language=ja&libraries=drawing,geometry,places,visualization"></script>
+    <script src="https://maps.google.com/maps/api/js?v=3&key={{ config('services.google-map.apikey') }}&language=ja&libraries=drawing,geometry,places,visualization"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
