5e3cee5416e41c24933ef3695f9cfcf2f8b3c3c2
Ibuki-Narukawa <ibuki0212job@gmail.com>
2021-12-31 05:15:27 +0900

    googl map api連携　変更

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'https://ride-share-2021.herokuapp.com/search/driverlist/*'
     ];
 }
